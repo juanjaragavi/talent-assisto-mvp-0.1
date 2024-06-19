d61838507c117ffed7718c4b61af2c9b8d57d6e5
Juan Jaramillo <info@juanjaramillo.tech>
2024-06-18 20:35:22 -0500

    ThreadID added to .env

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,5 +1,11 @@
-export let assistantId = "asst_7YOTAgWF4OPT7pJ4YodW2Tgz"; // set your assistant ID here
+export let assistantId = "asst_7YOTAgWF4OPT7pJ4YodW2Tgz";
 
 if (assistantId === "") {
   assistantId = process.env.OPENAI_ASSISTANT_ID;
+}
+
+export let threadId = "thread_3X0MATTAy1xAZgpE66Z1C0GT";
+
+if (threadId === "") {
+  threadId = process.env.OPENAI_THREAD_ID;
 }
