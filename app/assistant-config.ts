5c3f824ce1f0adf63264e723eab54708a19a67c4
Juan Jaramillo <info@juanjaramillo.tech>
2024-06-19 08:20:53 -0500

    Assistant and Thread ID modified

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,10 +1,10 @@
-export let assistantId = "asst_7YOTAgWF4OPT7pJ4YodW2Tgz";
+export let assistantId = "asst_4rYFGWjzcZ5DvsVfyWsa9a1W";
 
 if (assistantId === "") {
   assistantId = process.env.OPENAI_ASSISTANT_ID;
 }
 
-export let threadId = "thread_3X0MATTAy1xAZgpE66Z1C0GT";
+export let threadId = "thread_zvB8pXoKvP4kU8AiI4tyrwj0";
 
 if (threadId === "") {
   threadId = process.env.OPENAI_THREAD_ID;
