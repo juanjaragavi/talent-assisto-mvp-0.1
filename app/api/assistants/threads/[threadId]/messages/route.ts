d61b7772e3ebe8465deb6e09fac91b630fc1620d
Juan Jaramillo <info@juanjaramillo.tech>
2024-06-18 20:53:41 -0500

    `route.ts` optimized

--- app/api/assistants/threads/[threadId]/messages/route.ts
+++ app/api/assistants/threads/[threadId]/messages/route.ts
@@ -1,11 +1,16 @@
 import { assistantId } from "@/app/assistant-config";
 import { threadId } from "@/app/assistant-config";
 import { openai } from "@/app/openai";
+import { Request } from 'express';
 
 export const runtime = "nodejs";
 
+type CustomRequest = Request & {
+  json: () => Promise<any>;
+};
+
 // Send a new message to a thread
-export async function POST(request, {}) {
+export async function POST(request: CustomRequest, {}) {
   const { content } = await request.json();
 
   await openai.beta.threads.messages.create(threadId, {
