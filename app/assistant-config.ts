a9161d8c34cf8e39396d5771a7ebe18eb67555bc
Juan Jaramillo <info@juanjaramillo.tech>
2024-06-26 18:29:14 -0500

    Several files optimized and other files deleted

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,11 +1,2 @@
-export let assistantId = "asst_4rYFGWjzcZ5DvsVfyWsa9a1W";
-
-if (assistantId === "") {
-  assistantId = process.env.OPENAI_ASSISTANT_ID;
-}
-
-export let threadId = "thread_zvB8pXoKvP4kU8AiI4tyrwj0";
-
-if (threadId === "") {
-  threadId = process.env.OPENAI_THREAD_ID;
-}
+export let assistantId = process.env.OPENAI_ASSISTANT_ID;
+export let threadId = process.env.OPENAI_THREAD_ID;
--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,21 @@
 import React from "react";
 import styles from "./page.module.css";
 
-const Home = () => {
-  const categories = {
-    "Basic chat": "basic-chat",
-    "Function calling": "function-calling",
-    "File search": "file-search",
-    All: "all",
-  };
+import Chat from "./components/chat";
+import FileViewer from "./components/file-viewer";
 
+const Home = () => {
   return (
     <main className={styles.main}>
-      <div className={styles.title}>
-        Explore sample apps built with Assistants API
-      </div>
       <div className={styles.container}>
-        {Object.entries(categories).map(([name, url]) => (
-          <a key={name} className={styles.category} href={`/examples/${url}`}>
-            {name}
-          </a>
-        ))}
+        <div className={styles.column}>
+          <FileViewer />
+        </div>
+        <div className={styles.chatContainer}>
+          <div className={styles.chat}>
+            <Chat />
+          </div>
+        </div>
       </div>
     </main>
   );
