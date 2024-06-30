d1b165520d74e0b84cc77a4fcc12bbfcc26eb84b
Juan Jaramillo <info@juanjaramillo.tech>
2024-06-30 18:15:43 -0500

    Deleted the Create ThreadID Function

--- app/api/assistants/threads/[threadId]/messages/route.ts
+++ app/api/assistants/threads/[threadId]/messages/route.ts
@@ -1,7 +1,7 @@
 import { assistantId } from "@/app/assistant-config";
 import { threadId } from "@/app/assistant-config";
 import { openai } from "@/app/openai";
-import { Request } from 'express';
+import { Request } from "express";
 
 export const runtime = "nodejs";
 
@@ -12,8 +12,6 @@ type CustomRequest = Request & {
 // Send a new message to a thread
 export async function POST(request: CustomRequest, {}) {
   const { content } = await request.json();
-
-  await openai.beta.threads.retrieve(threadId);
 
   const stream = openai.beta.threads.runs.stream(threadId, {
     assistant_id: assistantId,
